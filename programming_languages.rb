0d5425e751b38b9e33020834f9e5cd14c67a743c
Shawn Koski <shawnnkoski@gmail.com>
2018-04-06 23:56:08 +0000

    Iterated and built nested hash programmatically

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,58 @@
+require 'pry'
+
+
+  languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
+
+
 def reformat_languages(languages)
-  # your code here
+  my_hash = {}
+  
+  languages.each do |language_style, name|
+    name.each do |name, attribute|
+      attribute.each do |attribute, type_value|
+        if my_hash.has_key? name
+          my_hash[name][:style] << language_style
+        else
+        my_hash[name] = {
+          type: type_value,
+          style: [language_style]
+        }
+      end
+       # binding.pry
+      end
+    end
+  end
+  my_hash
 end
+
+puts reformat_languages(languages)
